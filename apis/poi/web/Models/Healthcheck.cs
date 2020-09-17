52d30df31aa93b52d31f4816508cf5d70fce31b8
wyliegraham <wylie@tektite.com>
2020-09-17 17:33:30 -0400

    2.0!

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -9,7 +9,7 @@ namespace poi.Models
         {
             Message = "POI Service Healthcheck";
             Status = "Healthy Yes!";
-            Version = "1.0";
+            Version = "2.0";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
